a97a9bf972b82f074ffc33e68e430b998dfe16df
tglynn <timmyglynnv3@gmail.com>
2018-09-26 22:21:51 -0400

    push a bunch of COSC200

--- assignment2/assignment2.sh
+++ assignment2/assignment2.sh
@@ -1,3 +1,20 @@
-##!/bin/bash
+#!/bin/bash
 
-echo hello world new test
+students=$(cat students.txt | wc -l)
+echo number of students: $students
+percent=10
+
+
+n=$(( students / percent ))
+#n=$(echo "scale=4; $num_of_lines*$percent" | bc)
+echo $n students to be removed from program
+echo
+
+sort -n students.txt > newstudents.txt
+sed -i -e "1,${n}d" newstudents.txt 
+echo New Eligible Students:
+echo
+cat newstudents.txt
+
+
+
